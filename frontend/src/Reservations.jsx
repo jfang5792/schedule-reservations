b6985eaba856529459ea742d937515034fdebee5
Jenny Fang <jfang5792@gmail.com>
2024-09-27 22:15:45 -0700

    Create reservations component, need a few functions here. TODO: if user logs in, show reservations if any, if user tries to schedule reservation, prompt if unavail

---
+++ frontend/src/Reservations.jsx
@@ -0,0 +1,11 @@
+/* React Hooks */
+import { useState } from 'react';
+
+import Login from './Login'
+import NavbarComponent from './Navibar'
+
+export default function Reservations() {
+    const [reservations, setReservations] = useState("")
+
+
+}
