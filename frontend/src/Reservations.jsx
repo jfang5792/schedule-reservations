bc16ec90dfd833185ed3dbd00c66abb30da0fd0c
Jenny Fang <jfang5792@gmail.com>
2024-10-07 20:48:16 -0700

    Create new component for reservations availablle and or booked. TODO: fetch req connecting server endpoint

--- frontend/src/Reservations.jsx
+++ frontend/src/Reservations.jsx
@@ -5,7 +5,27 @@ import Login from './Login'
 import NavbarComponent from './Navibar'
 
 export default function Reservations() {
-    const [reservations, setReservations] = useState("")
+    const [times, setTimes] = useState(null);
+    const [selectedTime, setSelectedTime] = useState(null);
+    const [disableTime, setDisableTime] = useState(null);
 
+    const [reservations, setReservations] = useState("")
+    const [confirmTime, setConfirmedTime] = useState(false)
+
+    //handle action when `Confirm Time` btn is clicked
+    //save resy to db on user_id or username?
+    const handleConfirmTimeBtn = async (btnClick) => {
+        setTimes(btnClick);
+        setSelectedTime(btnClick);
+        setDisableTime(btnClick);
+        console.log('Confirming time --:', btnClick);
+        print('Confirming time selected:', btnClick)
+
+      }
+
+
+    return (
+        pass
+    )
 
 }
